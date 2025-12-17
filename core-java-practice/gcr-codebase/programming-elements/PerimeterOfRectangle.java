31ee5fee5e4a90c71dffef908a654b9eb88ef032
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-17 16:39:50 +0530

    [Bhaskar] Added solution for calculating perimeter of rectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PerimeterOfRectangle {
+    public static void main(String[] args){
+      Scanner scn = new Scanner(System.in);
+      double l = scn.nextDouble();
+      double b = scn.nextDouble();
+
+        double p = 2 * (l + b);
+
+        System.out.println(p);
+    }
+  
+}
