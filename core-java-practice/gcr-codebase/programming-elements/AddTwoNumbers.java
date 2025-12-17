c06316961b020a70a7dd9f38a30dbcde6b5bf24e
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-17 16:00:11 +0530

    [Bhaskar] Added solution for adding two numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+      Scanner scn  = new Scanner(System.in);
+      int n1 = scn.nextInt();
+      int n2 = scn.nextInt();
+
+        int sum = n1 + n2;
+
+        System.out.println(sum);
+    }
+  
+}
