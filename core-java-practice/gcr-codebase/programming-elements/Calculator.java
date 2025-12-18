a19f29808981b5c1dcebd5df3f8b71031bb19cd0
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-18 15:35:54 +0530

    [Bhaskar] Added solution for Basic Calculator

---
+++ core-java-practice/gcr-codebase/programming-elements/Calculator.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Calculator {
+  public static void main(String[] args){
+    Scanner scn= new Scanner(System.in);
+    float number1 = scn.nextFloat();
+    float number2 = scn.nextFloat();
+    double addition = number1 + number2;
+    double subtraction = number1 - number2;
+    double multiplication = number1 * number2;
+    double division = number1 / number2;
+    System.out.println("The addition, subtraction, multiplication and division value of 2 numbers"+" "+number1+"and"+number2+" "+"is"+" "+addition+" , "+subtraction+" , "+multiplication+" , "+division);
+
+
+  }
+  
+}
