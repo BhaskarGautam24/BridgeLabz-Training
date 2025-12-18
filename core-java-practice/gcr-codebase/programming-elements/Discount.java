b2c8f5db4ebef6988b86cef4589b470bd40d2a32
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-18 11:37:40 +0530

    [Bhaskar] Added solutio for finding discounted fee

---
+++ core-java-practice/gcr-codebase/programming-elements/Discount.java
@@ -0,0 +1,12 @@
+
+
+public class Discount {
+  public static void main(String[] args){
+    int fee = 125000;
+    discountpercent = 10;
+    int discount = (fee * discountpercent)/100;
+    int result = fee - discount;
+    System.out.println("The discount amount is INR"+" "+discount+" "+"and final discounted fee is INR"+" "+result);
+  }
+  
+}
