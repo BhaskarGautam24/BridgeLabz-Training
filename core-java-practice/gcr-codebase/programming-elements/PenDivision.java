17066b2380e9b360622a549fa5add6721c89ac74
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-18 11:29:05 +0530

    [Bhaskar] Added solutio for division of pens

---
+++ core-java-practice/gcr-codebase/programming-elements/PenDivision.java
@@ -0,0 +1,12 @@
+
+
+public class PenDivision {
+  public static void main(String[] args){
+    int total = 14;
+    int st = 3;
+    int div = total/st;
+    int rem = total%st;
+    System.out.println("The Pen Per Student is"+" "+div+" "+"and the remaining pen not distributed is"+" "+rem);
+  }
+  
+}
