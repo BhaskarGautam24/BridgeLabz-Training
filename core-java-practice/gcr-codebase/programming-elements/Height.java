5d0ac753d62feef3feaf89cd06e93f74967108c4
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-19 11:26:42 +0530

    [Bhaskar] Added updated solution for findingHeight

--- core-java-practice/gcr-codebase/programming-elements/Height.java
+++ core-java-practice/gcr-codebase/programming-elements/Height.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class Height{
   public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
-    double HeightInCentimeter = scn.nextDouble();
-    double HeightInInches = HeightInCentimeter / 2.54;
-    int HeightInFoots = (HeightInInches / 12);
-    System.out.println("Your Height in cm is"+" "+HeightInCentimeter+" "+"while in feet is"+" "+HeightInFoots+" "+"and inches is"+" "+HeightInInches);
+    double heightInCentimeter = scn.nextDouble();
+    double heightInInches = HeightInCentimeter / 2.54;
+    int heightInFoots = (HeightInInches / 12);
+    System.out.println("Your Height in cm is"+" "+heightInCentimeter+" "+"while in feet is"+" "+heightInFoots+" "+"and inches is"+" "+heightInInches);
 
 
     
