a00c9d6af537eb76650c0e51cc7f362477027322
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-20 23:04:23 +0530

    [Bhaskar] Add. Add program to find youngest and tallest friend using arrays

---
+++ core-java-practice/gcr-codebase/arrays/YoungestAndTallestFriend.java
@@ -0,0 +1,41 @@
+/*Create a program to find the youngest friends among 3 Amar, Akbar, and Anthony based on their ages and the tallest among the friends based on their heights
+Hint => 
+Take user input for age and height for the 3 friends and store it in two arrays each to store the values for age and height of the 3 friends
+Loop through the array and find the youngest of the 3 friends and the tallest of the 3 friends
+Finally display the youngest and tallest of the 3 friends
+ */
+package arrays;
+import java.util.*;
+public class YoungestAndTallestFriend {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        String[] friends = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+        // Input ages and heights
+        for (int i = 0; i < friends.length; i++) {
+            System.out.print("Enter age of " + friends[i] + ": ");
+            ages[i] = scn.nextInt();
+            System.out.print("Enter height of " + friends[i] + " in centimeters: ");
+            heights[i] = scn.nextDouble();
+        }
+        // Find youngest friend
+        int youngestIndex = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[youngestIndex]) {
+                youngestIndex = i;
+            }
+        }
+         // Find tallest friend
+        int tallestIndex = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[tallestIndex]) {
+                tallestIndex = i;
+            }
+        } 
+        // Display results
+        System.out.println("The youngest friend is: " + friends[youngestIndex] + " with age " + ages[youngestIndex]);
+        System.out.println("The tallest friend is: " + friends[tallestIndex] + " with height " + heights[tallestIndex] + " cm");
+    }
+}
