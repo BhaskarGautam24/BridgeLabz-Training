7b6de0c002e272ca807334052fd9953d809cf63f
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-20 11:30:22 +0530

    [Bhaskar] Add. Added solution to check whether a no. is armstrong or not

---
+++ core-java-practice/gcr-codebase/control-flows/ArmStrong.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class ArmStrong {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scn.nextInt();
+        int originalNumber = number;
+        int sum = 0;
+
+        while (originalNumber != 0) {
+            int digit = originalNumber % 10;
+            sum += Math.pow(digit, 3);
+            originalNumber /= 10;
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
+
