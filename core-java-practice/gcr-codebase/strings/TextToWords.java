5fb6ce4c7305460393cb0285cd6d51e4e8f49070
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-22 15:13:16 +0530

    Refactor. Refactored solution for ShortestAndLongestWord

--- core-java-practice/gcr-codebase/strings/TextToWords.java
+++ core-java-practice/gcr-codebase/strings/TextToWords.java
@@ -1,33 +1,19 @@
-/*Write a program to split the text into words, compare the result with the split() method and display the result 
-Hint => 
-Take user input using the Scanner nextLine() method 
-Create a Method to find the length of the String without using the built-in length() method. 
-Create a Method to split the text into words using the charAt() method without using the String built-in split() method and return the words. Use the following logic
-Firstly Count the number of words in the text and create an array to store the indexes of the spaces for each word in a 1D array
-Then Create an array to store the words and use the indexes to extract the words
-Create a method to compare the two String arrays and return a boolean
-The main function calls the user-defined method and the built-in split() method. Call the user defined method to compare the two string arrays and display the result */
-
 package strings;
 import java.util.*;
 public class TextToWords {
   public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
-    System.out.println("Enter a Text: ");
-    String text = scn.nextLine();
+    String text = scn.nextLine();                      // Read input text
     String[] wordsUsingCustomMethod = splitText(text);
     String[] wordsUsingBuiltInMethod = text.split(" ");
     boolean areEqual = compareArrays(wordsUsingCustomMethod, wordsUsingBuiltInMethod);              // Call the comparison method
 
     if(areEqual){
-      System.out.println("The two methods produce the same result.");
+      System.out.println("The methods produce the same result.");
     }
     else{
-      System.out.println("The two methods produce different results.");
+      System.out.println("The methods produce different results.");
     }
-
-    
-
   }
   public static int length(String text){
     int count=0;
@@ -51,7 +37,7 @@ public class TextToWords {
         wordCount++;
       }
     }
-    String[] words = new String[wordCount];
+    String[] words = new String[wordCount];                          // Create array to hold words
     int index=0;
     int start=0;
     for(int i=0;i<length;i++){
