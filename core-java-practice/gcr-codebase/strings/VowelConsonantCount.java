29318252e8546d274de6c00b4360ad63c1e89ae0
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-22 15:20:00 +0530

    Refactor. Refactored the solution for vowel and consonant count in string

--- core-java-practice/gcr-codebase/strings/VowelConsonantCount.java
+++ core-java-practice/gcr-codebase/strings/VowelConsonantCount.java
@@ -1,11 +1,3 @@
-/*Write a program to find vowels and consonants in a string and display the count of  Vowels and Consonants in the string
-Hint => 
-Create a method to check if the character is a vowel or consonant and return the result. The logic used here is as follows:
-Convert the character to lowercase if it is an uppercase letter using the ASCII values of the characters
-Check if the character is a vowel or consonant and return Vowel, Consonant, or Not a Letter
-Create a Method to Method to find vowels and consonants in a string using charAt() method and finally return the count of vowels and consonants in an array
-Finally, the main function takes user inputs, calls the user-defined methods, and displays the result.
- */
 package strings;
 import java.util.*;
 public class VowelConsonantCount {
@@ -17,7 +9,6 @@ public class VowelConsonantCount {
     System.out.println("Number of Vowels: " + counts[0]);
     System.out.println("Number of Consonants: " + counts[1]);
   }
-
   public static String checkCharType(char ch){
     if(ch >= 'A' && ch <= 'Z'){
       ch = (char)(ch + 32); // Convert to lowercase
@@ -32,7 +23,6 @@ public class VowelConsonantCount {
       return "Not a Letter";
     }
   }
-
   public static int[] countVowelsAndConsonants(String str){
     int vowelCount = 0;
     int consonantCount = 0;
