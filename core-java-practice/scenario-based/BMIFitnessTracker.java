8f78351a635872253d93c80668aceb9d50082955
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-22 10:44:04 +0530

    Refactor. Refactor the solution of BMIFitnessTraker

--- core-java-practice/scenario-based/BMIFitnessTracker.java
+++ core-java-practice/scenario-based/BMIFitnessTracker.java
@@ -2,20 +2,20 @@ import java.util.*;
 public class BMIFitnessTracker {
   public static void main(String[] args){
     Scanner scn = new Scanner(System.in);
-    System.out.println("Enter your height in centimeters: ");
+    System.out.println("Enter your height in centimeters:");
     double heightCm = scn.nextDouble();
-    System.out.println("Enter your weight in kilograms: ");
+    System.out.println("Enter your weight in kilograms:");
     double weightKg = scn.nextDouble();
     double heightM = heightCm / 100;                // convert height to meters
 
     double bmi = weightKg / (heightM * heightM);    // calculate BMI
 
     String Status;
-    if(bmi <= 18.4){
+    if(bmi <18.5){
       Status = "Underweight";
       System.out.println("You are underweight, please consult a nutritionist.");
     }
-    else if(bmi <= 24.9){
+    else if(bmi <25){
       Status = "Normal";
       System.out.println("You have a normal weight, keep up the good work!");
     }
@@ -24,5 +24,4 @@ public class BMIFitnessTracker {
       System.out.println("You are overweight, consider a fitness plan.");
     }  
   }
-  
 }
