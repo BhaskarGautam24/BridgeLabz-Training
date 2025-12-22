cb999f59fe1f7f6441cfe3fb5ac441d98ee64379
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-22 15:17:42 +0530

    Add. Added solution for vowel and consonant count in string

---
+++ core-java-practice/gcr-codebase/strings/VowelConsonantCount.java
@@ -0,0 +1,52 @@
+/*Write a program to find vowels and consonants in a string and display the count of  Vowels and Consonants in the string
+Hint => 
+Create a method to check if the character is a vowel or consonant and return the result. The logic used here is as follows:
+Convert the character to lowercase if it is an uppercase letter using the ASCII values of the characters
+Check if the character is a vowel or consonant and return Vowel, Consonant, or Not a Letter
+Create a Method to Method to find vowels and consonants in a string using charAt() method and finally return the count of vowels and consonants in an array
+Finally, the main function takes user inputs, calls the user-defined methods, and displays the result.
+ */
+package strings;
+import java.util.*;
+public class VowelConsonantCount {
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String str = scn.nextLine();
+    int[] counts = countVowelsAndConsonants(str);               // method works to count vowels and consonants
+    System.out.println("Number of Vowels: " + counts[0]);
+    System.out.println("Number of Consonants: " + counts[1]);
+  }
+
+  public static String checkCharType(char ch){
+    if(ch >= 'A' && ch <= 'Z'){
+      ch = (char)(ch + 32); // Convert to lowercase
+    }
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+      return "Vowel";
+    }
+    else if(ch >= 'a' && ch <= 'z'){
+      return "Consonant";
+    }
+    else{
+      return "Not a Letter";
+    }
+  }
+
+  public static int[] countVowelsAndConsonants(String str){
+    int vowelCount = 0;
+    int consonantCount = 0;
+    int length = str.length();
+    for(int i = 0; i < length; i++){
+      char ch = str.charAt(i);
+      String type = checkCharType(ch);
+      if(type.equals("Vowel")){
+        vowelCount++;
+      }
+      else if(type.equals("Consonant")){
+        consonantCount++;
+      }
+    }
+    return new int[]{vowelCount, consonantCount};
+  }
+}
