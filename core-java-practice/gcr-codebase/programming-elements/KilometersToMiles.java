c770f2a0604a42d6c34b0742819e3d180a1b9c16
Bhaskar Gautam <premprakash202123@gmail.com>
2025-12-23 12:43:32 +0530

    Add. Added the solution for conversion of kilometers to miles

--- core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
+++ core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
@@ -1,12 +1,9 @@
-
 import java.util.*;
 public class KilometersToMiles {
     public static void main(String [] args){
       Scanner scn = new Scanner(System.in);
-      double km = scn.nextDouble();
-
+      double Km = scn.nextDouble();
       double miles = km * 0.621371;
-      System.out.println(miles);
-      
+      System.out.println(miles);     
     }
 }
