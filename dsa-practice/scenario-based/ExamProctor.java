28e503c0746b2d8b4f856663571c1613e8273f69
Bhaskar Gautam <premprakash202123@gmail.com>
2026-01-19 12:48:35 +0530

    [Bhaskar Gutam] Add. Added the solution of ExamProctor.java

--- dsa-practice/scenario-based/ExamProctor.java
+++ dsa-practice/scenario-based/ExamProctor.java
@@ -1,32 +1,30 @@
-import java.util.HashMap;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class ExamProctor {
 
-    static Stack<Integer> navigationStack = new Stack<Integer>();
+    static Stack<Integer> navigation = new Stack<Integer>();
 
-    static HashMap<Integer, String> studentAnswers = new HashMap<Integer, String>();
+    static HashMap<Integer, String> ansOfStudent = new HashMap<Integer, String>();
 
     // fixed size array for correct answers
     static String[] correctAnswers = new String[6]; 
     // index = questionId (ignoring index 0)
 
     static void visitQuestion(int questionId) {
-        navigationStack.push(questionId);
+        navigation.push(questionId);
         System.out.println("Visited Question : " + questionId);
     }
 
     static void saveAnswer(int questionId, String answer) {
-        studentAnswers.put(questionId, answer);
+        ansOfStudent.put(questionId, answer);
         System.out.println("Answer saved for Question " + questionId);
     }
 
     static int calculateScore() {
         int score = 0;
 
-        for (int qId : studentAnswers.keySet()) {
-            if (studentAnswers.get(qId).equals(correctAnswers[qId])) {
+        for (int qId : ansOfStudent.keySet()) {
+            if (ansOfStudent.get(qId).equals(correctAnswers[qId])) {
                 score++;
             }
         }
